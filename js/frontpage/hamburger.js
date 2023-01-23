fba6c23e3e141d8aef181afcae3117562f534efd
Rasmus Eliasson <ras.muse@hotmail.com>
2023-01-23 21:43:28 +0100

    hamburger

---
+++ js/frontpage/hamburger.js
@@ -0,0 +1,13 @@
+export default function hamburger() {
+  let hamburgerIcon = document.querySelector("#hamburger-icon");
+  hamburgerIcon.addEventListener("click", () => {
+    let mainNavShow = document.querySelector(".main-nav");
+    if (hamburgerIcon.getAttribute("src") == "/static/hamburger-icon.svg") {
+      hamburgerIcon.setAttribute("src", "/static/close-icon.svg");
+      mainNavShow.classList.add("show");
+    } else {
+      hamburgerIcon.setAttribute("src", "/static/hamburger-icon.svg");
+      mainNavShow.classList.remove("show");
+    }
+  });
+}
